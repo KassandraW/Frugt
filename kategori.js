ea921deca1a8470db7f26b8222f0b7ab71a85547
KassandraW <kassandrawadum@hotmail.com>
2023-02-20 11:30:14 +0100

    js til kategori

--- kategori.js
+++ kategori.js
@@ -1,23 +1,14 @@
 fetch("https://tema7-f389.restdb.io/rest/frugter")
-      .then(res=>res.json())
-      .then(showCategories);
+  .then((res) => res.json())
+  .then(showCategories);
 
 function showCategories(cats) {
-cats.forEach(showCategory);
-
+  cats.forEach(showCategory);
 }
 
-function showCategory(cat){
-
-// fanger template 
-const template = document.querySelector("template").content;
-
-//cloner 
-const clone = template.cloneNode(true);
-//ændre indhold
-clone.querySelector("a").textContent = cat.category;
-clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
-
-//appender 
-document.querySelector(".letterGroup ol").appendChild(clone);
+function showCategory(cat) {
+  //ændre indhold
+  document.querySelector(
+    ".plante"
+  ).href = `frugter.html?category=${cat.udplantning}`;
 }
