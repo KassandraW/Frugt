6d089c6106ae7f6fab68b1d3c6deea07625945e3
KassandraW <119849798+KassandraW@users.noreply.github.com>
2023-02-21 12:14:16 +0100

    hmm

--- beskrivelse.js
+++ beskrivelse.js
@@ -1,6 +1,13 @@
 const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("id");
+const id = urlParams.get("_id");
 
-fetch("https://tema7-f389.restdb.io/rest/frugter/" + id)
-  .then((res) => res.json())
-  .then(showCategories);
+fetch("https://tema7-f389.restdb.io/rest/frugter/63e9f3d8aa8607500004f237")
+  .then((response) => response.json())
+  .then((data) => visFrugt(data));
+
+function visFrugt(frugt) {
+  console.log("visFrugt");
+
+  //ændre indhold
+  document.querySelector(".name").textContent = frugt.navn;
+}
